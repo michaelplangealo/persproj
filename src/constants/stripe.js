04b0832e3fd3ed94106bc22a2a74d31a01ff55f3
Michael Plange <michaelpplange@gmail.com>
2018-05-07 10:37:59 -0500

    still hosting

--- src/constants/stripe.js
+++ src/constants/stripe.js
@@ -1,6 +1,6 @@
 const STRIPE_PUBLISHABLE =
   process.env.NODE_ENV === "production"
-    ? "pk_live_MY_PUBLISHABLE_KEY"
+    ? "pk_test_HCNR31VaUZ3mwAro17figVEG"
     : "pk_test_HCNR31VaUZ3mwAro17figVEG";
 
 export default STRIPE_PUBLISHABLE;
