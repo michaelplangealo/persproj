6eee0c23b633c651be87ed0864302cbbbd5a675a
Michael Plange <michaelpplange@gmail.com>
2018-05-07 10:23:07 -0500

    hosting still

--- server/constants/stripe.js
+++ server/constants/stripe.js
@@ -2,8 +2,10 @@ const configureStripe = require("stripe");
 
 const STRIPE_SECRET_KEY =
   process.env.NODE_ENV === "production"
-    ? "sk_live_MY_SECRET_KEY"
+    ? "sk_test_dNQ30iGmbuKg9lujyQjBxkPI"
     : "sk_test_dNQ30iGmbuKg9lujyQjBxkPI";
+
+// replaced sk-live_MY_SECRET_KEY with sk_test_dNQ30iGmbuKg9lujyQjBxkPI
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
